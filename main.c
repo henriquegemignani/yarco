08d274deec50775997c65608c72a0aabd1d1ab86
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2010-04-20 18:22:22 +0000

    pasta headers e cabecalho do arquivo main.c
    git-svn-id: svn://svn/henriquelima/labprog@4 c5a4425f-b1d8-4b97-91a7-095824198b80

---
+++ main.c
@@ -0,0 +1,7 @@
+/*******************************************************************/
+/** MAC0211 - Laboratorio de Programacao I                        **/
+/** Projeto de Jogo                                               **/
+/**                                                               **/
+/** Nomes: (TODO: galera preecher)                                **/
+/** Henrique Gemignani Passos Lima      nUSP: 6879634             **/
+/*******************************************************************/
