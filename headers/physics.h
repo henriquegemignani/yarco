1e7b4f28c27e938af7a125fb92c794133dcdf325
Andre Mesquita Pereira <nikal.umoglich@gmail.com>
2010-04-20 18:50:05 +0000

    git-svn-id: svn://svn/henriquelima/labprog@9 c5a4425f-b1d8-4b97-91a7-095824198b80

---
+++ headers/physics.h
@@ -0,0 +1,11 @@
+#ifndef _PHYSICS_H_
+#define _PHYSICS_H_
+
+#include <stdlib.h>
+#include <math.h>
+#include "commom.h"
+
+vector newDirection( vector v );
+acceleration newAcceleration( acceleration a );
+
+#endif /* _PHYSICS_H_ */
