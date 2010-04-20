f290cef86e0483892c75e44307c569b3d75b3bb8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2010-04-20 18:58:08 +0000

    types.h inicado mais alguns arquivos
    git-svn-id: svn://svn/henriquelima/labprog@11 c5a4425f-b1d8-4b97-91a7-095824198b80

---
+++ headers/types.h
@@ -0,0 +1,17 @@
+/*******************************************************************/
+/** MAC0211 - Laboratorio de Programacao I                        **/
+/** Projeto de Jogo                                               **/
+/*******************************************************************/
+
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+struct Vector {
+  double x, y;
+};
+
+typedef struct Vector vector;
+typedef struct Vector velocity;
+typedef struct Vector acceleration;
+
+#endif /* _TYPES_H_ */
