7c7d1d9a4267da0963052fc38fc7b2c53a195403
Renan Teruo Carneiro <renanteruoc@gmail.com>
2010-04-20 18:29:08 +0000

    teste?
    git-svn-id: svn://svn/henriquelima/labprog@6 c5a4425f-b1d8-4b97-91a7-095824198b80

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 /** MAC0211 - Laboratorio de Programacao I                        **/
 /** Projeto de Jogo                                               **/
 /**                                                               **/
-/** Nomes: (TODO: galera preecher)								  **/	
-/** Andre Mesquita Pereira				nUSP: 6879530			  **/
+/** Nomes: (TODO: galera preecher)				  **/	
+/** Andre Mesquita Pereira		nUSP: 6879530		  **/
 /** Henrique Gemignani Passos Lima      nUSP: 6879634             **/
+/** Renan Teruo Carneiro                nUSP: 6514157             **/
 /*******************************************************************/
