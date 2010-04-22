44552f06d2d36b223268494d4bd4337ccbe2128f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2010-04-22 00:21:28 +0000

    bugs arrumados
    git-svn-id: svn://svn/henriquelima/labprog@40 c5a4425f-b1d8-4b97-91a7-095824198b80

--- lib/graphics_terminal.c
+++ lib/graphics_terminal.c
@@ -3,6 +3,7 @@
 /** Projeto de Jogo                                               **/
 /*******************************************************************/
 
+#include "common.h"
 #include "graphics.h"
 #include "person.h"
 #include "persontable.h"
@@ -30,7 +31,7 @@ char tokenToChar( TOKEN t ) {
 #define PIXEL_SIZE_X 20
 #define PIXEL_SIZE_Y 15
 
-static TOKEN[OUTPUT_HEIGHT][OUTPUT_WIDTH] outputScreen;
+static TOKEN outputScreen[OUTPUT_HEIGHT][OUTPUT_WIDTH];
 
 int graphicInitialize() {
     int i, j;
@@ -40,8 +41,8 @@ int graphicInitialize() {
     return 0;
 }
 
-void grahpicUpdatePerson(person p) {
-    point p = personGetPos(p);
+void grahpicUpdatePerson(person per) {
+    point p = personGetPos(per);
     int x = p.x / PIXEL_SIZE_X, y = p.y / PIXEL_SIZE_Y;
     if( x < OUTPUT_WIDTH && y < OUTPUT_HEIGHT )
         outputScreen[y][x] = PASSENGER;
@@ -58,9 +59,9 @@ int grahpicUpdate() {
 
 int graphicDraw() {
     int i, j;
-    for( i = 0; i < OUTPUT_HEIGHT; i++ )
+    for( i = 0; i < OUTPUT_HEIGHT; i++ ) {
         for( j = 0; j < OUTPUT_WIDTH; j++ )
-            printf("%c", tokenToChar(outputScreen[i][j]);
+            printf("%c", tokenToChar(outputScreen[i][j]));
         printf("\n");
     }
     return 0;
