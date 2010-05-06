273504e59e4466cea8314267f5a61d4d49ad3dcd
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2010-05-06 02:37:09 +0000

    Mudança em como se acessa valores padrões, talvez de para formalizar um pouco mais isso. Também fiz uma limpa de comentários não necessários.
    git-svn-id: svn://svn/henriquelima/labprog@82 c5a4425f-b1d8-4b97-91a7-095824198b80

--- main.c
+++ main.c
@@ -16,47 +16,57 @@
 #include <time.h>
 #include <string.h>
 
-void argRead(int argc, char **argv, double *defaultSpeed, double *createRate, int *debugMode, int *repetitions);
-/*Le argumentos e seta variaveis de acordo com os mesmos*/
+struct Configuration {
+    double defaultSpeed;
+    double createRate;
+    int debugMode;
+    int repetitions;
+    int pedantic; /* morre em qualquer erro? */
+};
+
+void argRead(int argc, char **argv, struct Configuration *defaults );
+/* Le argumentos e seta variaveis de acordo com os mesmos */
 
 int main(int argc, char ** argv){
-  int i, debugMode = 0, repetitions = 100;
-  double defaultSpeed = PERSON_SPEED_DEFAULT, createRate = PERSON_CREATE_RATE_DEFAULT;
+  struct Configuration defaults;
+  int i;
+  
+  defaults.defaultSpeed = PERSON_SPEED_DEFAULT;
+  defaults.createRate = PERSON_CREATE_RATE_DEFAULT;
+  defaults.debugMode = 0;
+  defaults.repetitions = 50;
+  defaults.pedantic = 1;
 
-  argRead(argc, argv, &defaultSpeed, &createRate, &debugMode, &repetitions); 
-  /* Separado por questoes de clareza do codigo. */
-  /* Possivelmente: criar uma funcao que verifica se um argumento especifico existe, para mais elegancia, embora seja menos eficiente */
-  /* Alias, o numero maximo de pasageiros tambem deveria ser customizavel ou non? */
+  argRead(argc, argv, &defaults); 
+  /* Separado por questoes de clareza do codigo. 
+  Possivelmente: criar uma funcao que verifica se um argumento especifico existe, 
+    para mais elegancia, embora seja menos eficiente.
+  Alias, o numero maximo de pasageiros tambem deveria ser customizavel ou non? */
 
-  /*Inicializa tabela de passageiros*/
-  personTableInit( defaultSpeed, createRate );
+  /* Inicializa tabela de passageiros */
+  personTableInit( defaults.defaultSpeed, defaults.createRate );
   srand( time( NULL ) );
   for( i = 0; i < PERSON_NUM_INIT; i++ )
-    if( personTableAddNew() == ERROR_PERSON_LIMIT_EXCEEDED ) {
-      //printf("Erro: limite de naufragos atingido!\n");
-      //exit(1);
+    if( personTableAddNew() == ERROR_PERSON_LIMIT_EXCEEDED )
       genError("Erro: limite de naufragos atingido!\n");
-      /*AVISO: genError sai do programa*/
-    }
-  /*Inicializa parte grafica*/
+      /* AVISO: genError sai do programa */
+  
+  /* Inicializa parte grafica */
   graphicInitialize();
-  // i = 1;
-  //printf("HELL, IT'S LOOPING TIME\n");
-  //while(i) {
-  for( i = 0; i < repetitions; i++){
+
+  for( i = 0; i < defaults.repetitions; i++){
     printf("\n\nIteracao: %d\n\n", i+1);
     personTableUpdate();
     graphicUpdate();
-	if( debugMode )
+	if( defaults.debugMode )
 		personTableDump();
 	else
 		graphicDraw();
-	//scanf(" %d", &i);
   }
   return 0;
 }
 
-void argRead(int argc, char **argv, double *defaultSpeed, double *createRate, int *debugMode, int *repetitions){
+void argRead(int argc, char **argv, struct Configuration *defaults ) {
   int i;
   for( i = 1; i < argc; i++ ) {
     if( strcmp( argv[i], "-h" ) == 0 || strcmp( argv[i], "--help" ) == 0 ) {
@@ -66,33 +76,47 @@ void argRead(int argc, char **argv, double *defaultSpeed, double *createRate, in
 	     "--speed: Determina a velocidade media dos passageiros. Padrao: %3.2f\n"
 	     "--rate: Determina o periodo entre 2 novos passageiros. Padrao: %3.2f\n"
 	     "--debug: Mostra posicao, velocidade e aceleracao de cada passageiro, ignorando parte grafica\n"
-	     "--repetitions: Define quantas iteracoes o programa mostrara. Padrao: %d\n",
-	     argv[0], *defaultSpeed, *createRate, *repetitions );
+	     "--repetitions: Define quantas iteracoes o programa mostrara. Padrao: %d\n"
+         "--pedantic: Define se o programa para a execucao em erros. Padrao: %s\n",
+	     argv[0], defaults->defaultSpeed, defaults->createRate, defaults->repetitions,
+            defaults->pedantic ? "True" : "False"
+         );
       exit(0);/*Ohnoes exit no meio do programa! <_< */
     }
     if( strncmp( argv[i], "--speed", 7 ) == 0 ) { /* supoe que usuario colocou um numero */
-      if( strlen(argv[i]) > 7 ) /* parametro da forma '--speedX' */ /*Jura que vai suportar parametro da forma speedX? Isso vai complicar na hora de dar suporte a argumento da forma -s X, que, IMO, é mais importante. E vai ter que dar suporte a -sX se a gente fizer isso... Especialmente agravante dado que ha suporte a -h.~Miojo*/
-        *defaultSpeed = atof(argv[i] + 7);
+      if( strlen(argv[i]) > 7 ) /* parametro da forma '--speedX' */ 
+    /* Jura que vai suportar parametro da forma speedX? Isso vai complicar na hora de dar 
+    suporte a argumento da forma -s X, que, IMO, é mais importante. E vai ter que dar 
+    suporte a -sX se a gente fizer isso... Especialmente agravante dado que ha suporte a -h.
+        ~Miojo*/
+        defaults->defaultSpeed = atof(argv[i] + 7);
       else   /* parametro da forma '--speed X' */
-        *defaultSpeed = atof(argv[++i]);
+        defaults->defaultSpeed = atof(argv[++i]);
       continue;
     }
     if( strncmp( argv[i], "--rate", 6 ) == 0 ) {
       if( strlen(argv[i]) > 6 )
-        *createRate = atof(argv[i] + 6);
+        defaults->createRate = atof(argv[i] + 6);
       else
-        *createRate = atof(argv[++i]);
+        defaults->createRate = atof(argv[++i]);
       continue;
     }
     if( strncmp( argv[i], "--debug", 7 ) == 0 ) {
-      *debugMode = 1;
+      defaults->debugMode = 1;
       continue;
     }
     if( strncmp( argv[i], "--repetitions", 13 ) == 0 ) {
       if( strlen(argv[i]) > 13 )
-        *repetitions = atoi(argv[i] + 13);
+        defaults->repetitions = atoi(argv[i] + 13);
       else
-        *repetitions = atoi(argv[++i]);
+        defaults->repetitions = atoi(argv[++i]);
+      continue;
+    }
+    if( strncmp( argv[i], "--pedantic", 10 ) == 0 ) {
+      if( strlen(argv[i]) > 10 )
+        defaults->pedantic = atoi(argv[i] + 13);
+      else
+        defaults->pedantic = atoi(argv[++i]);
       continue;
     }
   }
