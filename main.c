151fa7ca6093d7777e386f7a5eff48f39c7be9b7
Renan Teruo Carneiro <renanteruoc@gmail.com>
2010-05-06 21:50:41 +0000

    pedantic nukado, adicionada opcao pause
    git-svn-id: svn://svn/henriquelima/labprog@88 c5a4425f-b1d8-4b97-91a7-095824198b80

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ struct Configuration {
     double createRate;
     int debugMode;
     int repetitions;
-    int pedantic; /* morre em qualquer erro? */
+  int pause;
 };
 
 void argRead(int argc, char **argv, struct Configuration *defaults );
@@ -37,7 +37,7 @@ int main(int argc, char ** argv){
   defaults.createRate = PERSON_CREATE_RATE_DEFAULT;
   defaults.debugMode = 0;
   defaults.repetitions = 50;
-  defaults.pedantic = 1;
+  defaults.pause = 0;
 
   argRead(argc, argv, &defaults); 
   /* Separado por questoes de clareza do codigo. 
@@ -60,69 +60,17 @@ int main(int argc, char ** argv){
     printf("\n\nIteracao: %d\n\n", i+1);
     personTableUpdate();
     graphicUpdate();
-	if( defaults.debugMode )
-		personTableDump();
-	else
-		graphicDraw();
+    if( defaults.debugMode )
+      personTableDump();
+    else
+      graphicDraw();
+    if( defaults . pause ){
+      printf("Aperte Enter para continuar...\n");
+      while( getchar() != '\n');
+    }
   }
   return 0;
 }
-
-/*void argRead(int argc, char **argv, struct Configuration *defaults ) {
-  int i;
-  for( i = 1; i < argc; i++ ) {
-    if( strcmp( argv[i], "-h" ) == 0 || strcmp( argv[i], "--help" ) == 0 ) {
-      printf("Usage: %s [OPTIONS]\n\n"
-	     "Opcoes: --speed, --rate, --help, -h, --debug, --repetitions\n"
-	     "--help, -h: Imprime isso e sai\n"
-	     "--speed: Determina a velocidade media dos passageiros. Padrao: %3.2f\n"
-	     "--rate: Determina o periodo entre 2 novos passageiros. Padrao: %3.2f\n"
-	     "--debug: Mostra posicao, velocidade e aceleracao de cada passageiro, ignorando parte grafica\n"
-	     "--repetitions: Define quantas iteracoes o programa mostrara. Padrao: %d\n"
-             "--pedantic: Define se o programa para a execucao em erros. Padrao: %s\n",
-	     argv[0], defaults->defaultSpeed, defaults->createRate, defaults->repetitions,
-            defaults->pedantic ? "True" : "False"
-         );
-	 exit(0);*//*Ohnoes exit no meio do programa! <_< *//*
-    }
-    if( strncmp( argv[i], "--speed", 7 ) == 0 ) { *//* supoe que usuario colocou um numero *//*
-       if( strlen(argv[i]) > 7 )*/ /* parametro da forma '--speedX' */ 
-    /* Jura que vai suportar parametro da forma speedX? Isso vai complicar na hora de dar 
-    suporte a argumento da forma -s X, que, IMO, é mais importante. E vai ter que dar 
-    suporte a -sX se a gente fizer isso... Especialmente agravante dado que ha suporte a -h.
-    ~Miojo*//*
-        defaults->defaultSpeed = atof(argv[i] + 7);
-	else  */ /* parametro da forma '--speed X' *//*
-        defaults->defaultSpeed = atof(argv[++i]);
-      continue;
-    }
-    if( strncmp( argv[i], "--rate", 6 ) == 0 ) {
-      if( strlen(argv[i]) > 6 )
-        defaults->createRate = atof(argv[i] + 6);
-      else
-        defaults->createRate = atof(argv[++i]);
-      continue;
-    }
-    if( strncmp( argv[i], "--debug", 7 ) == 0 ) {
-      defaults->debugMode = 1;
-      continue;
-    }
-    if( strncmp( argv[i], "--repetitions", 13 ) == 0 ) {
-      if( strlen(argv[i]) > 13 )
-        defaults->repetitions = atoi(argv[i] + 13);
-      else
-        defaults->repetitions = atoi(argv[++i]);
-      continue;
-    }
-    if( strncmp( argv[i], "--pedantic", 10 ) == 0 ) {
-      if( strlen(argv[i]) > 10 )
-        defaults->pedantic = atoi(argv[i] + 13);
-      else
-        defaults->pedantic = atoi(argv[++i]);
-      continue;
-    }
-  }
-}*/
 
 void argRead (int argc, char **argv, struct Configuration *defaults ){
   char *argValue;
@@ -134,22 +82,21 @@ void argRead (int argc, char **argv, struct Configuration *defaults ){
 	   "--rate, -r: Determina o periodo entre 2 novos passageiros. Padrao: %3.2f\n"
 	   "--debug, -d: Mostra posicao, velocidade e aceleracao de cada passageiro, ignorando parte grafica\n"
 	   "--repetitions, -R: Define quantas iteracoes o programa mostrara. Padrao: %d\n"
-	   "--pedantic, -p: Define se o programa para a execucao em erros. Padrao: %s\n",
-	   argv[0], defaults->defaultSpeed, defaults->createRate, defaults->repetitions,
-	   defaults->pedantic ? "True" : "False"
+	   "--pause, -p: Determina que o programa pausara a cada iteracao.\n",
+	   argv[0], defaults->defaultSpeed, defaults->createRate, defaults->repetitions
 	   );
     exit(0);/*Ohnoes exit no meio do programa! <_< */
   }
   if( argFind( argc, argv, "--debug", "-d") )
     defaults -> debugMode = 1;    
+  if( argFind( argc, argv, "--pause", "-p") )
+    defaults -> pause = 1;
   if( ( argValue = argVal( argc, argv, "--rate", "-r") ) )
     defaults -> createRate = atof( argValue );
   if( ( argValue = argVal( argc, argv, "--speed", "-s") ) )
     defaults -> defaultSpeed = atof( argValue );
   if( ( argValue = argVal( argc, argv, "--repetitions", "-R" ) ) )
     defaults -> repetitions = atoi( argValue );
-  if( ( argValue = argVal( argc, argv, "--pedantic", "-p") ) )
-    defaults -> pedantic = atoi( argValue );
 }
 
 int argFind(int argc, char **argv, char *argLong, char *argShort){
