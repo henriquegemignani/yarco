8649fdb5192ad11cb7326c5525c3aeaf26e3a7d5
Andre Mesquita Pereira <nikal.umoglich@gmail.com>
2010-05-20 20:48:59 +0000

    graphics.c inicializado, embora nao haja nenhuma mudanca estrutural no resto do programa
    git-svn-id: svn://svn/henriquelima/labprog@126 c5a4425f-b1d8-4b97-91a7-095824198b80

--- lib/graphics.c
+++ lib/graphics.c
@@ -3,12 +3,49 @@
 /** Projeto de Jogo                                               **/
 /*******************************************************************/
 
+#include "common.h"
+#include "object.h"
 #include "graphics.h"
+#include "person.h"
+#include "persontable.h"
+#include "physics.h"
+#include <allegro.h>
 
-/*<_____________________________________________________<*/
-/* trololol */
-static int nonemptier;
-void trololler()
+BITMAP *buffer;
+
+void graphicInitialize( int mode )
 {
-    nonemptier = 5;
+	allegro_init();
+	set_color_depth(32);
+	if( mode == WINDOWED_MODE )
+		set_gfx_mode( GFX_AUTODETECT_WINDOWED, 	SCREEN_SIZE_X, SCREEN_SIZE_Y, 0, 0 );
+	else if( mode == FULLSCREEN_MODE )
+		set_gfx_mode( GFX_AUTODETECT_FULLSCREEN, SCREEN_SIZE_X, SCREEN_SIZE_Y, 0, 0 );
+
+	buffer = create_bitmap( SCREE_SIZE_X, SCREEN_SIZE_Y	);
+}
+
+void graphicUpdate( personTable table )
+{
+	clear( buffer );
+	personTableExecute( table, graphicUpdatePerson );
+}
+
+void graphicUpdatePerson( person per )
+{
+	BITMAP tmp = create_bitmap( 15, 15 );
+	point p = personGetPos( per );
+	rectfill( tmp, 0, 0, 15, 15, 3000 ); /* essa linha ainda vai perecer */
+	draw_sprite( buffer, tmp, p.x, p.y );
+}
+
+void graphicDraw()
+{
+	draw_sprite( screen, buffer, 0, 0 );
+}
+
+void graphicFinish()
+{
+	destroy_bitmap( buffer );
+	allegro_exit();
 }
--- lib/graphics.h
+++ lib/graphics.h
@@ -8,17 +8,20 @@
 
 #include "common.h"
 #include "persontable.h"
+#include <allegro.h>
 
 typedef struct GraphicEngine *graphicEngine;
 
 #define SCREEN_SIZE_X 800       //MAX_X     //800
 #define SCREEN_SIZE_Y 600       //MAX_Y     //600
+#define WINDOWED_MODE 1
+#define FULLSCREEN_MODE 2
 
 /*Inicializa parte grafica. uniquePerson define se deve usar um caracter diferente 
 para cada pessoa desenhada.*/
-void graphicInitialize(personTable table);
+void graphicInitialize( int mode );
 /*Atualiza os buffers*/
-void graphicUpdate();
+void graphicUpdate( personTable table );
 /*Mostra o que tem que ser mostrado*/
 void graphicDraw();
 /*Libera a memoria da parte grafica*/
