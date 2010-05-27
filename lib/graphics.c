10aae04b993f94f052566ab2ae74389c5ef17b02
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2010-05-27 00:58:49 +0000

    graphicUpdateObject honra a cor do objeto na hora de desenhar.
    git-svn-id: svn://svn/henriquelima/labprog@167 c5a4425f-b1d8-4b97-91a7-095824198b80

--- lib/graphics.c
+++ lib/graphics.c
@@ -38,10 +38,10 @@ void graphicUpdateObject(object per)
     rectfill(tmp, 0, 0, per->radius * 2, per->radius * 2, 0xff00ff);    /*TODO (graphicUpdateObject): algo que verifique qual o modo grafico para determinar qual a cor transparente */
     switch (per->tex.type) {
     case TEX_CIRCLE:
-        circlefill(tmp, per->radius, per->radius, per->radius, 0);
+        circlefill(tmp, per->radius, per->radius, per->radius, per->tex.color);
         break;
     case TEX_SQUARE:
-        rectfill(tmp, 0, 0, per->radius * 2, per->radius * 2, 0);
+        rectfill(tmp, 0, 0, per->radius * 2, per->radius * 2, per->tex.color);
         break;
         /* a parte dos barcos vai ser punk */
     case TEX_ISOSC_TRIANGLE:
@@ -53,7 +53,7 @@ void graphicUpdateObject(object per)
             per->radius-(per->radius*sin(vectorAngle(per->acc)+(4*PI/5))), 
             per->radius-(per->radius*cos(vectorAngle(per->acc)-(4*PI/5))), 
             per->radius-(per->radius*sin(vectorAngle(per->acc)-(4*PI/5))), 
-            0x000000);
+            per->tex.color);
        /* rotate_sprite(buffer, tmp, p.x, p.y, per->acc.y / (2 * PI) * 256);*/
         /*OH CEUS  TODO: implementar o giramento de barquinhos */
         /*TODO: Ver se funfa. >_> */
